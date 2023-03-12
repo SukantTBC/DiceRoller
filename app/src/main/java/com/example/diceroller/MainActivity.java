1dba3f28935468b9ffbbfb9e9d1077faeff696f0
Sukant Rajkarnikar <57744317+Carrothead547@users.noreply.github.com>
2023-03-12 10:27:12 +0545

    Dice Roller Lab Assignment

---
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.diceroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button rollButton = findViewById(R.id.button);
+        TextView resultTextView = findViewById(R.id.textView);
+
+
+        rollButton.setOnClickListener(
+                view -> {
+                    Toast DiceRollToast = Toast.makeText(MainActivity.this, "Dice Rolling...", Toast.LENGTH_SHORT);
+                    DiceRollToast.show();
+                    int number = new Random().nextInt(6);
+                    number+=1;
+                    resultTextView.setText("6");
+                    resultTextView.setText(String.valueOf(number));
+                }
+        );
+
+    }
+}
